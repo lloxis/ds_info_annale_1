fd077085e10f4ad04ab9837fb9bc45299775fdf4
lloxis <48058753+lloxis@users.noreply.github.com>
2020-11-26 13:30:09 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -40,3 +40,47 @@ def ind_moins(liste, a):
         if liste[i] < a:
             indices_liste.append(i)
     return indices_liste
+
+# exo 7
+def moyenne_jour(liste):
+    n = len(liste)
+    liste_moyenne_jour = []
+    for i in range(7):
+        liste_moyenne_jour.append(moyenne(liste[i:n:7]))
+    return liste_moyenne_jour
+
+# exo 8
+def maximum(liste):
+    """
+    Renvoie la valeur du maximum puis l'indice du maxium
+    """
+    n = len(liste)
+    maxi = liste[0]
+    indice_max = 0
+    for i in range(1, n):
+        if liste[i] > maxi:
+            maxi = liste[i]
+            indice_max = i
+    return maxi, indice_max
+
+
+jours = ["dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"]
+def jour_max(liste):
+    # indice_max est l'indice du jour qui a la plus grande moyenne de réservation
+    indice_max = maximum(moyenne_jour(liste))[1]
+    return jours[indice_max]
+
+# exo 16
+arriveesO = [495, 562, 740, 841, 920, 1264]
+departO = [624, 803, 950, 1047, 1133, 1320]
+def nb_quais(arrivees, departs):
+    nb_quais_neccessaires = 0
+    max_nb_quais_neccessaires = 0
+    for i in range(arrivees[0], departs[-1]+1):
+        if i in arrivees:
+            nb_quais_neccessaires +=1
+        elif i in departO:
+            nb_quais_neccessaires -= 1
+        if nb_quais_neccessaires > max_nb_quais_neccessaires:
+            max_nb_quais_neccessaires = nb_quais_neccessaires
+    return max_nb_quais_neccessaires
